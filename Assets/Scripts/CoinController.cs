f6b1c65223b978133a1cd1bdb64f744e5708a5bc
Ket <ketcodesgames@gmail.com>
2025-10-18 22:35:27 -0700

    Add SFX

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    [SerializeField] AudioClip collectSound;
+
+    bool wasCollected = false;
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Player") && !wasCollected)
         {
+            wasCollected = true;
+
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
             Destroy(gameObject);
         }
     }
